ba6610cec8f8a5170f249cefc9cb4a4202a27395
stefan-scott <scottst@spsd.sk.ca>
2025-11-04 13:28:44 -0600

    starter done

--- 21 2D Array Basics/sketch.js
+++ 21 2D Array Basics/sketch.js
@@ -24,6 +24,43 @@ function setup() {
 function draw() {
   background(220);
   renderGrid();
+  print(getCurrentX(),getCurrentY());
+}
+
+function mousePressed(){
+  //flip current tile
+  //upgrade: only do this if the mouse is on Canvas
+  
+  let x = getCurrentX();
+  let y = getCurrentY();
+
+  //ALWAYS: flip the "focused" tile
+  flip(x,y);
+
+  //IF THEY EXIST:
+  //flip our NSEW neighbours (cross pattern)
+  if(x+1 < cols) flip(x+1,y);
+  if(y-1 >= 0) flip(x, y-1);
+
+}
+
+function getCurrentX(){
+  //determine current col of mouse position
+  let constrainedX = constrain(mouseX, 0, width-1);
+  return floor(constrainedX / squareSize);
+}
+
+function getCurrentY(){
+  //determine current row of mouse position
+  let constrainedY = constrain(mouseY, 0, height-1);
+  return floor(constrainedY / squareSize);
+}
+
+
+function flip(x,y){
+  //takes a tile @ x,y and inverts its value
+  if(grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
 }
 
 function renderGrid(){
