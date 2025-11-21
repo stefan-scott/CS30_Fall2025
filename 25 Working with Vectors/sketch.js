14cb6bc5eabe5a9ad2cd96da2b9d439e564a31db
stefan-scott <scottst@spsd.sk.ca>
2025-11-21 13:37:13 -0600

    wip

---
+++ 25 Working with Vectors/sketch.js
@@ -0,0 +1,13 @@
+// Working with Vectors
+// Mr. Scott
+// Nov 21, 2025
+// Useful for modelling forces...
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(200);
+}
